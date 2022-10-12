83d9c20d31c7e28ba15237917e06c1c6e9a4130e
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-10-12 17:26:53 +0300

    Update D-Strange apples.cpp

--- 1st lesson/D-Strange apples.cpp
+++ 1st lesson/D-Strange apples.cpp
@@ -9,9 +9,11 @@ int main() {
     cin >> n >> s;
     vector<int> a(n, 0);
     vector<int> b(n, 0);
+    vector<int> d(n, 0);
     vector<int> m(0);
     for (int i = 0; i < n; i++) {
         cin >> a[i] >> b[i];
+        d[i] = i;
         if (b[i] - a[i] > 0) {
             c = a[i];
             a[i] = a[p];
@@ -19,10 +21,17 @@ int main() {
             c = b[i];
             b[i] = b[p];
             b[p] = c;
+            c = d[i];
+            d[i] = d[p];
+            d[p] = c;
             p++;
         }
     }
     c = 0;
+    for (int i = 0; i < n; i++){
+        cout << a[i] << " " << b[i] << ' ' << b[i] - a[i] << endl;
+    }
+    cout << "p = " << p << endl;
     while (t < p) {
         if (s - a[c % n] >= 0) {
             s -= a[c % n] - b[c % n];
@@ -40,17 +49,21 @@ int main() {
     t = 0;
     for (int i = 0; i < 1100; i++) {
         for (int j = p; j < n; j++) {
-            if (b[j] - a[i] == i) {
-                m.push_back(j);
-                a[j] = 100000000;
-            } else {
-                cout << "-1";
-                return 0;
+            if (b[j] - a[j] == -i) {
+                if (s - a[j] >= 0) {
+
+                    s -= a[j] + b[j];
+                    m.push_back(j);
+                    a[j] = 100000000;
+                } else {
+                    cout << "-1";
+                    return 0;
+                }
             }
         }
     }
     for (int i = 0; i < m.size(); i++) {
-        cout << m[i] << ' ';
+        cout << m[d[i]] + 1 << ' ';
     }
 
 
