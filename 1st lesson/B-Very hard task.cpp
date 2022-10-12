7829ab504d9a19be1a6f32146619c96d84448cad
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-10-12 08:38:53 +0300

    Create B-Very hard task.cpp

---
+++ 1st lesson/B-Very hard task.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, n, k, q, a, b, c;
+    cin >> n >> k >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> a >> b;
+        t = 0;
+        while (true) {
+            if (a < b) {
+                c = a;
+                a = b;
+                b = c;
+            }
+            if (a == b) {
+                cout << t << endl;
+                break;
+            }
+            a = (a + k - 2) / k;
+            t++;
+        }
+    }
+}
