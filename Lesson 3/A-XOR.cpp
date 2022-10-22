156fc29b3279bfff77642c4d2d7d25e5a26b6834
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-10-22 19:28:00 +0300

    Update A-XOR.cpp

--- Lesson 3/A-XOR.cpp
+++ Lesson 3/A-XOR.cpp
@@ -12,17 +12,16 @@ using str = string;
 int main() {
     int t = 0, n, mn;
     cin >> t;
-    for (int ti = 0; ti < t; ti++){
+    for (int ti = 0; ti < t; ti++) {
         mn = 1000000000;
         cin >> n;
-        vector <int> a(n);
-        for (int i = 0; i < n; i++){
+        vector<int> a(n);
+        sort(a.begin(), a.end());
+        for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < i; j++){
-                mn = min(mn, (a[i]^a[j]));
-            }
+        for (int i = 0; i < n - 1; i++) {
+            mn = min(mn, (a[i] ^ a[i + 1]));
         }
         cout << mn << endl;
     }
