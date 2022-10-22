b0543b69b91acacabf81cd90410e9a0e7ed1f51a
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-10-22 20:40:20 +0300

    Update B-Painting of Barriers.cpp

--- Lesson 3/B-Painting of Barriers.cpp
+++ Lesson 3/B-Painting of Barriers.cpp
@@ -11,13 +11,14 @@ using ld = double;
 using str = string;
 
 int main() {
-    ll n, mn, s = 0, p;
+    ll n, mn = 19999999999;
     cin >> n;
     vector <ll> h(n);
     for (int i = 0; i < n; i++){
         cin >> h[i];
     }
     sort(h.begin(), h.end(), [](int x, int y){return x > y;});
+    mn = min(h[0], n);
     for (int i = 0; i < n - 1; i++){
         mn = min(mn, i + 1 + h[i + 1]);
     }
