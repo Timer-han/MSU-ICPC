f932726e757387ac649f3a2942c3f6f031102a9d
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-10-22 18:57:42 +0300

    Create A-XOR.cpp

---
+++ Lesson 3/A-XOR.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+using ll = long long;
+using ld = double;
+using str = string;
+
+int main() {
+    int t = 0, n, mn;
+    cin >> t;
+    for (int ti = 0; ti < t; ti++){
+        mn = 1000000000;
+        cin >> n;
+        vector <int> a(n);
+        for (int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < i; j++){
+                mn = min(mn, (a[i]^a[j]));
+            }
+        }
+        cout << mn;
+    }
+
+
+    return 0;
+}
