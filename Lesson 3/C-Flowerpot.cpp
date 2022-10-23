bf7da6cd3746eb4a3b1637b8d1b79d57c59cdd61
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-10-23 15:42:57 +0300

    Update C-Flowerpot.cpp

--- Lesson 3/C-Flowerpot.cpp
+++ Lesson 3/C-Flowerpot.cpp
@@ -11,29 +11,23 @@ using ld = double;
 using str = string;
 
 int main() {
-    ll n, d, mn = 19999999999, x = 0;
+    ll n, d, mn = 19999999999;
     cin >> n >> d;
     vector<pair<ll, ll>> h(n);
     for (int i = 0; i < n; i++) {
         cin >> h[i].first >> h[i].second;
     }
-    sort(h.begin(), h.end(), [](auto &x, auto &y) {
-        return x.second < y.second;
-    });
-    sort(h.begin(), h.end(), [](auto &x, auto &y) {
-        return x.first < y.first;
-    });
-
-    for (int i = 1; i < n - 1; i++) {
-        if (h[x].second - h[i].second >= d) {
-            cout << h[x].first - h[i].first << endl;
-            return 0;
-        }
-        if (h[x].first != h[i].first) {
-            x = i;
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            if (h[j].second - h[i].second >= d){
+                mn = min(mn, abs(h[i].first - h[j].first));
+            }
         }
     }
-
-    cout << -1;
+    if (mn == 19999999999){
+        cout << -1;
+        return 0;
+    }
+    cout << mn;
     return 0;
 }
