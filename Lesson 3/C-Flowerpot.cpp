6af5993127754fc1b8fed6c59f81a81cce9b7011
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-10-23 16:14:06 +0300

    Update C-Flowerpot.cpp

--- Lesson 3/C-Flowerpot.cpp
+++ Lesson 3/C-Flowerpot.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
+#include <chrono>
 #include <algorithm>
 #include <queue>
 
@@ -11,23 +13,31 @@ using ld = double;
 using str = string;
 
 int main() {
+
     ll n, d, mn = 19999999999;
     cin >> n >> d;
     vector<pair<ll, ll>> h(n);
     for (int i = 0; i < n; i++) {
         cin >> h[i].first >> h[i].second;
     }
-    for (int i = 0; i < n; i++){
-        for (int j = i + 1; j < n; j++){
-            if (abs(h[j].second - h[i].second) >= d){
-                mn = min(mn, abs(h[i].first - h[j].first));
-            }
+    auto start = chrono::system_clock::now();
+    sort(h.begin(), h.end(), [](auto &x, auto &y) {
+        return x.first < y.first;
+    });
+    for (int i = 1; i < n - 1; i++){
+        for (int j = 0; j < n - i; j++){
+            mn = min(mn, abs(h[j].first - h[j + i].first));
+        }
+        if (mn != 19999999999){
+            cout << mn << endl;
+            cout << chrono::duration<double>(chrono::system_clock::now() - start).count();
+            return 0;
         }
     }
     if (mn == 19999999999){
         cout << -1;
-        return 0;
     }
-    cout << mn;
+    cout << mn << endl;
+    cout << chrono::duration<double>(chrono::system_clock::now() - start).count();
     return 0;
 }
