30ace740554816670be1c084163d4edc40ea9a79
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-11-21 21:26:20 +0300

    Create C-Painting.cpp

---
+++ Lesson 6/C-Painting.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <chrono>
+#include <algorithm>
+#include <queue>
+#include <iomanip>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+using ll = long long;
+using ld = double;
+using str = string;
+
+int main() {
+    int n, k = 1, f;
+    cin >> n;
+//    auto start = chrono::system_clock::now();
+    vector <int> b(0);
+    vector <int> a(n);
+    for (int i = 2; i <= n + 1; i++){
+        vector <int> c(0);
+        f = 0;
+        for (int j : b){
+            if (i % j == 0){
+                f++;
+            }
+        }
+        if (f == 0){
+            b.push_back(i);
+            cout << 1 << " ";
+        } else {
+            cout << 2 << " ";
+        }
+    }
+//    cout << endl;
+//    cout << chrono::duration<double>(chrono::system_clock::now() - start).count() << endl;
+    return 0;
+}
