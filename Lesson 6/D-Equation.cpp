f92ddf676548dfdb21b5696c75c1ae051549d0e2
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-11-21 22:25:07 +0300

    Create D-Equation.cpp

---
+++ Lesson 6/D-Equation.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <chrono>
+#include <algorithm>
+#include <queue>
+#include <iomanip>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+using ll = long long;
+using ld = double;
+using str = string;
+
+int main() {
+    ll t, a, b, p;
+    cin >> t;
+    auto start = chrono::system_clock::now();
+    for (; t > 0; t--) {
+        cin >> a >> b >> p;
+        a = ((a % p) + p) % p;
+        b = ((b % p) + p) % p;
+        if (a == 0) {
+            if (b == 0) {
+                cout << "Any" << endl;
+            } else {
+                cout << -1 << endl;
+            }
+            continue;
+        }
+        for (int i = 0; i < p; i++) {
+            if ((a * i + b) % p == 0) {
+                cout << i << endl;
+                break;
+            }
+        }
+    }
+
+    cout << chrono::duration<double>(chrono::system_clock::now() - start).count() << endl;
+    return 0;
+}
