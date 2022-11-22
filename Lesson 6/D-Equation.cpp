57d1a305ce892e78953cd5517f13537349f315e6
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-11-22 15:01:17 +0300

    Update D-Equation.cpp

--- Lesson 6/D-Equation.cpp
+++ Lesson 6/D-Equation.cpp
@@ -15,14 +15,37 @@ using ll = long long;
 using ld = double;
 using str = string;
 
+ll f(ll x, ll y, ll* a) {
+    ll z = y, k = 0, p = x;
+    while (y != 1) {
+        a[k] = x / y;
+        k++;
+        x %= y;
+        swap(x, y);
+    }
+    x = 1;
+    y = a[k - 1];
+    for (ll i = k - 2; i >= 0; i--) {
+        x += a[i] * y;
+        swap(x, y);
+    }
+    if ((y * z) % p == 1) {
+        return y;
+    }
+    return -y;
+}
+
+
 int main() {
-    ll t, a, b, p, k;
+    ll t, a, b, p, k = 0, h;
+    ll m[100000];
     cin >> t;
 //    auto start = chrono::system_clock::now();
     for (; t > 0; t--) {
         cin >> a >> b >> p;
+        h = p;
         a = ((a % p) + p) % p;
-        b = ((b % p) + p) % p;
+        b = (((-b) % p) + p) % p;
         if (a == 0) {
             if (b == 0) {
                 cout << "Any" << "\n";
@@ -31,20 +54,11 @@ int main() {
             }
             continue;
         }
-        pair <int, int> x(1, 0);
-        pair <int, int> y(0, 1);
-        pair <int, int> z;
-        while (a != 1){
-            z.first = x.first; z.second = x.second;
-            k = a;
-            x.first -= y.first * (p / a); x.second -= y.second * (p / a);
-            a = p % a;
-            y.first = z.first; y.second = z.second;
-            p = k;
-        }
-        cout << ((x.first * b) % p + p) % p << "\n";
+        h = f(p, a, m);
+        cout << (h * b % p + p) % p << "\n";
+
     }
 
-//    cout << chrono::duration<double>(chrono::system_clock::now() - start).count() << endl;
+//    cout << chrono::duration<double>(chrono::system_clock::now() - start).count() << "\n";
     return 0;
 }
