e0967ffa09ed0bdbfa299b8064997957e740c53e
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-11-22 14:58:58 +0300

    Update D-Equation.cpp

--- Lesson 6/D-Equation.cpp
+++ Lesson 6/D-Equation.cpp
@@ -15,59 +15,34 @@ using ll = long long;
 using ld = double;
 using str = string;
 
-ll invert(ll a, ll m)
-{
-    if (a < 1 or m < 2)
-        return -1;
-
-    ll u1 = m;
-    ll u2 = 0;
-    ll v1 = a;
-    ll v2 = 1;
-
-    while (v1 != 0)
-    {
-        ll q = u1 / v1;
-        ll t1 = u1 - q*v1;
-        ll t2 = u2 - q*v2;
-        u1 = v1;
-        u2 = v2;
-        v1 = t1;
-        v2 = t2;
-    }
-
-    return u1 == 1 ? (u2 + m) % m : -1;
-}
-
-
-
 int main() {
-    ll t, a, b, p, k = 0, h;
-    ll m[100000];
+    ll t, a, b, p, k;
     cin >> t;
 //    auto start = chrono::system_clock::now();
     for (; t > 0; t--) {
         cin >> a >> b >> p;
-        h = p;
         a = ((a % p) + p) % p;
-        b = (((-b) % p) + p) % p;
+        b = ((b % p) + p) % p;
         if (a == 0) {
             if (b == 0) {
-                cout << "Any" << endl;
+                cout << "Any" << "\n";
             } else {
-                cout << -1 << endl;
+                cout << -1 << "\n";
             }
             continue;
-        } else if (a == 1) {
-            cout << b;
-            continue;
-        } else if (a == -1) {
-            cout << -b + p;
-            continue;
         }
-        h = invert(a, p);
-        cout << (h * b % p + p) % p << endl;
-
+        pair <int, int> x(1, 0);
+        pair <int, int> y(0, 1);
+        pair <int, int> z;
+        while (a != 1){
+            z.first = x.first; z.second = x.second;
+            k = a;
+            x.first -= y.first * (p / a); x.second -= y.second * (p / a);
+            a = p % a;
+            y.first = z.first; y.second = z.second;
+            p = k;
+        }
+        cout << ((x.first * b) % p + p) % p << "\n";
     }
 
 //    cout << chrono::duration<double>(chrono::system_clock::now() - start).count() << endl;
