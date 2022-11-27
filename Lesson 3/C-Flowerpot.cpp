1d4d6eff2d3daaaec818f39e1a2cf2d2f2424835
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-11-27 19:03:21 +0300

    Update C-Flowerpot.cpp

--- Lesson 3/C-Flowerpot.cpp
+++ Lesson 3/C-Flowerpot.cpp
@@ -6,6 +6,8 @@
 #include <algorithm>
 #include <queue>
 #include <iomanip>
+#include <set>
+#include <cmath>
 
 using namespace std;
 
@@ -13,32 +15,86 @@ using ll = long long;
 using ld = double;
 using str = string;
 
-int main() {
 
-    ll n, d, mn = 19999999999;
-    cin >> n >> d;
-    vector<pair<ll, ll>> h(n);
-    for (int i = 0; i < n; i++) {
-        cin >> h[i].first >> h[i].second;
-    }
-    auto start = chrono::system_clock::now();
-    sort(h.begin(), h.end(), [](auto &x, auto &y) {
-        return x.second < y.second;
-    });
-    for (int i = 0; i < n; i++){
-        for (int j = n - 1; j > i; j--){
-            if (h[j].second - h[i].second < d){
-                break;
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int t, n, mn, mni, mn2, mni2, mx, mxi;
+    cin >> t;
+//    auto start = chrono::system_clock::now();
+    for (; t > 0; t--) {
+        cin >> n;
+        vector<int> a(n);
+        vector<vector<int>> b;
+        cin >> a[0] >> a[1];
+
+
+        mn = mn2 = mx = a[1];
+        mni = mni2 = mxi = 1;
+        for (int i = 2; i < n; i++) {
+            cin >> a[i];
+            if (a[i] <= mn2) {
+                mn2 = a[i];
+                mni2 = i;
             }
-            mn = min(mn, abs(h[j].first - h[i].first));
+            if (a[i] >= mx) {
+                mx = a[i];
+                mxi = i;
+                mni = mni2;
+                mn = mn2;
+            }
         }
-    }
-    if (mn != 19999999999){
-        cout << mn << endl;
-    } else {
-        cout << -1 << endl;
+        if (a[0] > mn - mx) {
+            a[0] = mn - mx;
+            b.push_back({1, mni + 1, mxi + 1});
+        }
+        for (int i = 1; i < n - 2; i++) {
+            if (i == mni && mni + 1 == mxi) {
+                mxi++;
+                mx = a[mxi];
+                mn2 = mx;
+                mni2 = mxi;
+                for (int j = mxi + 1; j < n; i++) {
+                    if (a[j] <= mn2) {
+                        mn2 = a[j];
+                        mni2 = j;
+                    }
+                    if (a[j] >= mx) {
+                        mx = a[j];
+                        mxi = j;
+                        mni = mni2;
+                        mn = mn2;
+                    }
+                }
+            } else if (i == mni) {
+                mni++;
+                mn = a[mni];
+                for (int j = mni + 1; j < mxi; j++) {
+                    if (mn >= a[j]) {
+                        mn = a[j];
+                        mni = j;
+                    }
+                }
+            }
+            if (a[i] > mn - mx) {
+                a[i] = mn - mx;
+                b.push_back({i + 1, mni + 1, mxi + 1});
+            }
+        }
+
+        if (a[n - 1] < a[n - 2]) {
+            cout << -1 << "\n";
+            continue;
+        }
+        cout << b.size() << "\n";
+        for (auto i: b) {
+            for (auto j: i) {
+                cout << j << " ";
+            }
+            cout << "\n";
+        }
+
     }
 //    cout << chrono::duration<double>(chrono::system_clock::now()-start).count();
-
     return 0;
 }
