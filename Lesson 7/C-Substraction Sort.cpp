2e47b04090ff1ea43f2dd327c7d648a25b445996
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-11-28 19:59:29 +0300

    Update C-Substraction Sort.cpp

--- Lesson 7/C-Substraction Sort.cpp
+++ Lesson 7/C-Substraction Sort.cpp
@@ -19,82 +19,38 @@ using str = string;
 int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
-    int t, n, mn, mni, mn2, mni2, mx, mxi;
+    int t;
     cin >> t;
-//    auto start = chrono::system_clock::now();
-    for (; t > 0; t--) {
+    for (int i = 0; i < t; ++i) {
+        int n;
         cin >> n;
-        vector<int> a(n);
-        vector<vector<int>> b;
-        cin >> a[0] >> a[1];
-
-
-        mn = mn2 = mx = a[1];
-        mni = mni2 = mxi = 1;
-        for (int i = 2; i < n; i++) {
-            cin >> a[i];
-            if (a[i] <= mn2) {
-                mn2 = a[i];
-                mni2 = i;
-            }
-            if (a[i] >= mx) {
-                mx = a[i];
-                mxi = i;
-                mni = mni2;
-                mn = mn2;
-            }
+        vector<int> arr(n);
+        for (int j = 0; j < n; ++j) {
+            cin >> arr[j];
         }
-        if (a[n - 1] < a[n - 2]) {
-            cout << -1 << "\n";
-            continue;
-        }
-        if (a[0] > mn - mx) {
-            a[0] = mn - mx;
-            b.push_back({1, mni + 1, mxi + 1});
-        }
-        for (int i = 1; i < n - 2; i++) {
-            if (i == mni && mni + 1 == mxi) {
-                mxi++;
-                mx = a[mxi];
-                mn2 = mx;
-                mni2 = mxi;
-                for (int j = mxi + 1; j < n; i++) {
-                    if (a[j] <= mn2) {
-                        mn2 = a[j];
-                        mni2 = j;
-                    }
-                    if (a[j] >= mx) {
-                        mx = a[j];
-                        mxi = j;
-                        mni = mni2;
-                        mn = mn2;
-                    }
+        if (arr[n - 2] > arr[n - 1]) {
+            cout << -1;
+            cout << endl;
+        } else {
+            if (arr[n - 1] < 0) {
+                if (is_sorted(arr.begin(), arr.end())) {
+                    cout << 0;
+                    cout << endl;
+                } else {
+                    cout << -1;
+                    cout << endl;
                 }
-            } else if (i == mni) {
-                mni++;
-                mn = a[mni];
-                for (int j = mni + 1; j < mxi; j++) {
-                    if (mn >= a[j]) {
-                        mn = a[j];
-                        mni = j;
-                    }
+            } else {
+                cout << n - 2;
+                cout << endl;
+                for (int k = 1; k < n - 1; ++k) {
+                    cout << n - k - 1 << ' ' << n - k << ' ' << n;
+                    cout << endl;
                 }
             }
-            if (a[i] > mn - mx) {
-                a[i] = mn - mx;
-                b.push_back({i + 1, mni + 1, mxi + 1});
-            }
         }
-
-        cout << b.size() << "\n";
-        for (auto i: b) {
-            for (auto j: i) {
-                cout << j << " ";
-            }
-            cout << "\n";
-        }
-
     }
+//    auto start = chrono::system_clock::now();
 //    cout << chrono::duration<double>(chrono::system_clock::now()-start).count();
     return 0;
 }
