8f2d819a6ce247c3364fb74ee96085fd9e68f3d9
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-11-29 17:53:26 +0300

    Update A-Revaccination.cpp

--- Lesson 7/A-Revaccination.cpp
+++ Lesson 7/A-Revaccination.cpp
@@ -18,23 +18,50 @@ using str = string;
 
 int main() {
     cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int t, n, m, mx = 0;
+    ios::sync_with_stdio(false);
+    int t, k;
     cin >> t;
-    for (; t > 0; t--){
+    while (t--) {
+        int n, m;
         cin >> n >> m;
-        vector <vector<int>> a(m, (vector <int> (3)));
-        for (int i = 0; i < n; i++){
-            cin >> a[i][1] >> a[i][2];
-            a[i][0] = a[i][1] + a[i][2];
-            mx = max(mx, a[i][1]);
+        vector<pair<int, int> > a(m);
+        for (int i = 0; i < m; ++i) {
+            cin >> a[i].first >> a[i].second;
         }
         sort(a.begin(), a.end(), [](auto x, auto y){
-            return x[0] > y[0];
+            return x.first >= y.first;
         });
-        for (int i = 0; i < n; i++){
-            if ()
+        vector<ll> dr(m + 1);
+        for (int i = 0; i < m; ++i) {
+            dr[i + 1] = dr[i] + a[i].first;
         }
+        ll ans = 0;
+        for (int i = 0; i < m; ++i) {
+            int stone = a[i].second;
+            int l = -1, r = m;
+            while (r - l > 1) {
+                int md = (l + r) / 2;
+                if (a[md].first >= stone) {
+                    l = md;
+                } else {
+                    r = md;
+                }
+            }
+            int cnt = l + 1;
+            cnt = min(cnt, n - 1);
+            if (cnt <= i) {
+                ans = max(ans, dr[cnt] + a[i].first + (ll) a[i].second * (n - 1 - cnt));
+            }
+        }
+        ll sum = 0;
+        int max_b = 0;
+        for (int i = 0; i < min(n, m); ++i) {
+            sum += a[i].first;
+            max_b = max(max_b, a[i].second);
+            k = sum + (ll) (n - i - 1) * max_b;
+            ans = (ans > k ? ans : k);
+        }
+        cout << ans << '\n';
     }
 
 
