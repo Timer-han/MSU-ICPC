aecac8b2f8ffbe2ecb2e8cbdee6da5cfbd77d8ee
Timer-han <111784360+Timer-han@users.noreply.github.com>
2022-12-03 20:45:13 +0300

    Update E-Preparations for the semifinals.cpp

--- Lesson 8/E-Preparations for the semifinals.cpp
+++ Lesson 8/E-Preparations for the semifinals.cpp
@@ -20,10 +20,10 @@ int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     int n, c;
+    ll d1 = 0, d2;
     cin >> n >> c;
     vector <ll> a(n);
     vector <ll> b(n);
-    vector <ll> d(n, 0);
     vector <ll> t(n, 0);
     for (int i = 0; i < n - 1; i++){
         cin >> a[i];
@@ -32,29 +32,30 @@ int main() {
         cin >> b[i];
     }
     if (a[0] < b[0] + c){
-        d[1] = a[0];
+        d2 = a[0];
     }else {
-        d[1] = b[0] + c;
+        d2 = b[0] + c;
         t[1] = 1;
     }
-    cout << 0 << " " << d[1] << " ";
+    cout << 0 << " " << d2 << " ";
     for (int i = 2; i < n; i++){
+        d1 = d2;
         if (t[i - 1]){
             if (a[i - 1] < b[i - 1]){
-                d[i] = a[i - 1] + d[i - 1];
+                d2 = a[i - 1] + d1;
             }else {
-                d[i] = b[i - 1] + d[i - 1];
+                d2 = b[i - 1] + d1;
                 t[i] = 1;
             }
         } else {
-            if (a[i - 1] <= b[i - 1] + c){
-                d[i] = a[i - 1] + d[i - 1];
+            if (a[i - 1] < b[i - 1] + c){
+                d2 = a[i - 1] + d1;
             }else {
-                d[i] = b[i - 1] + c + d[i - 1];
+                d2 = b[i - 1] + c + d1;
                 t[i] = 1;
             }
         }
-        cout << d[i] << " ";
+        cout << d2 << " ";
     }
 //    auto start = chrono::system_clock::now();
 //    cout << chrono::duration<double>(chrono::system_clock::now()-start).count();
